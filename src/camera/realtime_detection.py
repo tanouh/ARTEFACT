f5c98c0f52cd3eb028a58e8e61369c4f1d69343c
zhiying <zhiying.zou@telecom-paris.fr>
2023-11-21 10:02:53 +0100

    fix dict

--- src/camera/realtime_detection.py
+++ src/camera/realtime_detection.py
@@ -5,40 +5,43 @@ from cv2 import aruco as arU
 import sys
 from dictlib import ARUCO_DICT
 
+
+dict = cv2.aruco.DICT_6X6_50
+
 def detect_aruco_tags(video_source=0):
     print("[INFO] starting video stream...")
     vc = cv2.VideoCapture(video_source)
     time.sleep(2.0)
 
+    arucoDict = arU.getPredefinedDictionary(dict)
+    arucoParams = arU.DetectorParameters()
+    detector = arU.ArucoDetector(arucoDict, arucoParams)
+
+
     while True:
         ret, frame = vc.read()
         if not ret:
             break
-        for dict in ARUCO_DICT :
-            arucoDict = arU.getPredefinedDictionary(ARUCO_DICT[dict])
-            arucoParams = arU.DetectorParameters()
-            detector = arU.ArucoDetector(arucoDict, arucoParams)
+     
 
-
-            (corners, ids, rejected) = detector.detectMarkers(frame)
-            if ids == None:
-                continue
-            print(ids)
-            if len(corners) > 0:
-                ids = ids.flatten()
-                for (markerCorner, markerID) in zip(corners, ids):
-                    corners = markerCorner.reshape((4, 2))
-                    (topLeft, topRight, bottomRight, bottomLeft) = corners
-                    topRight = (int(topRight[0]), int(topRight[1]))
-                    bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-                    bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-                    topLeft = (int(topLeft[0]), int(topLeft[1]))
+        (corners, ids, rejected) = detector.detectMarkers(frame)
+        if ids == None:
+            continue
+        print(ids)
+        if len(corners) > 0:
+            ids = ids.flatten()
+            for (markerCorner, markerID) in zip(corners, ids):
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
+                topRight = (int(topRight[0]), int(topRight[1]))
+                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                topLeft = (int(topLeft[0]), int(topLeft[1]))
                     
                     # TO DO : (FILL ME) Get distance approximation 
                     # Etape 1 : Calibrer la caméra cf doc
-                    height = topRight[1] - bottomRight[1]
-                    print("Height of ArUco marker: ", height)
-                    print("Type: ", dict)
+                height = topRight[1] - bottomRight[1]
+                print("Height of ArUco marker: ", height)
 
 
                     # Etape 2: 
