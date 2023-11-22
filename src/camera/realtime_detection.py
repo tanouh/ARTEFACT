5fbb9e1d43dc99b0f3f23eaea94ca14b5efc8965
tania.mahandry <tania.mahandry@telecom-paris.fr>
2023-11-22 17:40:16 +0100

    set dist func

--- src/camera/realtime_detection.py
+++ src/camera/realtime_detection.py
@@ -15,8 +15,8 @@ vhstep = 0.05
 def get_distance(height):
     if height <= 0 : 
         return 0 
-    elif 0 < height and height <= 300 :
-        return 13793/(height**1.07)
+    # elif 0 < height and height <= 300 :
+    #     return 13793/(height**1.07)
     else :
         return -0.0814*height + 53.412
 
