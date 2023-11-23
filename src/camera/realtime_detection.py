018d733241fb94baec58c2a728df5666d08b5310
tania.mahandry <tania.mahandry@telecom-paris.fr>
2023-11-23 09:34:03 +0100

    adjusting speed

--- src/camera/realtime_detection.py
+++ src/camera/realtime_detection.py
@@ -98,7 +98,7 @@ class Detector():
 
                         mc.modify_speed(motor, 70) # modifier la vitesse 
                         mc.turn_left(motor)
-                        time.sleep(sec + 5*step)
+                        time.sleep(sec)
 
                         mc.stop_motor(motor)
                         mc.move_forward(motor)
