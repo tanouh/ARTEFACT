9f309cbc99cba7f9cf1482bf2e05d367d114be46
zhiying <zhiying.zou@telecom-paris.fr>
2023-12-07 13:57:45 +0100

    video stream on web

--- src/motor/app.py
+++ src/motor/app.py
@@ -21,7 +21,7 @@ def launch_streaming():
         time.sleep(0.5)
         global motor
         if not auto_mode : 
-                return streamer.streaming(motor, None)
+                return streamer.streaming(motor, detector.detect_aruco_tags)
         else :
                 detector = rd.Detector()
                 return streamer.streaming(motor, detector.detect_aruco_tags)
