d3bc1b4dc09a8081f373065fdc1a2082012522b3
tania.mahandry <tania.mahandry@telecom-paris.fr>
2023-12-14 11:05:00 +0100

    fix

--- src/camera/stream_cam.py
+++ src/camera/stream_cam.py
@@ -9,8 +9,7 @@ class Streamer():
         def __init__(self):
                 self.camera = cv2.VideoCapture(0)
                 self.camera.set(cv2.CAP_PROP_BUFFERSIZE, 1)
-                fps = self.camera.set(cv2.CAP_PROP_FPS, 20)
-                print(f"La fréquence d'images par seconde (FPS) de la caméra est : {fps}")
+                self.camera.set(cv2.CAP_PROP_FPS, 20)
 
 
                
