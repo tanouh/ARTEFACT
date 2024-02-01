a5f3ed72c808a05d26716cde8e5985f11119c4a0
Alexandre Mallez <alexandre.mallez@telecom-paris.fr>
2024-02-01 15:47:59 +0100

    camera test

--- src/camera/stream_cam.py
+++ src/camera/stream_cam.py
@@ -46,6 +46,58 @@ class Streamer():
                 self.camera.set(cv2.CAP_PROP_FPS, 30)
                 self.camera.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
+               
+        # def streaming (self,motor, func):
+        #         print("[STREAM] starting video stream...")
+        #         while True :
+        #                 ret, frame = self.camera.read()
+        #                 if not ret:
+        #                         break
+        #                 if func is not None :
+        #                         func(frame, motor)
+        #                 else : 
+        #                         print("[STREAM] No function to read")
+
+        #         print("[STREAM] ending video stream...")
+        #         cv2.destroyAllWindows()
+        #         self.camera.release()
+
+        def generate_frames(self, motor, func):
+                print("[STREAM] Starting video stream...")
+                while True:
+                        ret, frame = self.camera.read()
+                        if not ret:
+                                break
+                        if func is not None:
+                                func(frame, motor)  # 处理图像的函数，如果需要的话
+
+                        ret, buffer = cv2.imencode('.jpg', frame)
+                        if not ret:
+                                break
+
+                        frame_bytes = buffer.tobytes()
+                        yield (b'--frame\r\n'
+                                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+        def streaming(self, motor, func):
+                if func is None:
+                        return self.generate_frames_without_processing(motor)
+                else:
+                        return self.generate_frames(motor, func)
+
+        def generate_frames_without_processing(self, motor):
+                print("[STREAM] Starting video stream without processing...")
+                while True:
+                        ret, frame = self.camera.read()
+                        if not ret:
+                                break
+                        ret, buffer = cv2.imencode('.jpg', frame)
+                        if not ret:
+                                break
+                        frame_bytes = buffer.tobytes()
+                        yield (b'--frame\r\n'
+                                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
         def release(self):
                 print("[STREAM] Ending video stream...")
                 cv2.destroyAllWindows()
