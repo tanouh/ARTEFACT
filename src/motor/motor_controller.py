e9f668154d3b1574347116da799fc0b4eecbdc09
Tania Mahandry <tania.mahandry@telecom-paris.fr>
2024-02-02 12:32:26 +0000

    Update file motor_controller.py

--- src/motor/motor_controller.py
+++ src/motor/motor_controller.py
@@ -14,6 +14,7 @@ def set_speed(motor, new_speed):
         if new_speed > 1 : 
                 motor.set_speed_right(new_speed)
                 motor.set_speed_left(new_speed)
+                print(motor.speed)
         if new_speed <= 1 :
                 motor.set_speed_right(100*new_speed)
                 motor.set_speed_left(100*new_speed)
